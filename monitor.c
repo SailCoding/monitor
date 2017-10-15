17ff089840058631c9e6e9f584b7bd97db29bb17
LboAnn <extend1994@gmail.com>
2017-10-15 17:00:08 +0800

    Build memory monitor to display memory usage

--- monitor.c
+++ monitor.c
@@ -5,11 +5,12 @@
 
 int main(){
   FILE *fp;
-  char read_buf[100], cpu_name[20];
+  char read_buf[100], cpu_name[20], mem_unit[5];
   int  lc = 0; //loop counter
   long last_cpu_stat[7], cpu_stat[7], last_cpu_used, cpu_used;
+  long mem_total, mem_free, mem_ava, mem_buf, mem_cache;
   long last_cpu_total = 0, cpu_total = 0;
-  double cpu_usage;
+  double cpu_usage, mem_usage;
   while(1){
     // cpu monitor
     fp = fopen("/proc/stat","r");
@@ -38,8 +39,19 @@ int main(){
     cpu_used = cpu_stat[0]+cpu_stat[1]+cpu_stat[2];
     cpu_usage = (double)(cpu_used-last_cpu_used)/(cpu_total-last_cpu_total)*100;
 
+    // memory monitor
+    fp = fopen("/proc/meminfo","r");
+    fscanf(fp,"%s %ld %s",read_buf,&mem_total,mem_unit);
+    fscanf(fp,"%s %ld %s",read_buf,&mem_free,mem_unit);
+    fscanf(fp,"%s %ld %s",read_buf,&mem_ava,mem_unit);
+    fscanf(fp,"%s %ld %s",read_buf,&mem_buf,mem_unit);
+    fscanf(fp,"%s %ld %s",read_buf,&mem_cache,mem_unit);
+    fclose(fp);
+    mem_usage = (double)(mem_total-mem_free-mem_buf-mem_cache)/mem_total*100;
+
     // print stats
     printf("CPU usage: %lf%c\n", cpu_usage, 37);
+    printf("MEM usage: %lf%c\n", mem_usage, 37);
 
     lc = 0;
   }
